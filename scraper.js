8d3753c847363bce85804eff95cd4909e49952e1
CedrickOlmsted99 <52431255+CedrickOlmsted99@users.noreply.github.com>
2023-06-01 02:54:34 -0400

    cleaned up comments

--- scraper.js
+++ scraper.js
@@ -1,44 +1,3 @@
-/*
-    //Instructions
-    -/ Copy this code
-    -/ Find an account whos followers you want to scrape
-    -/ Paste this into the console
-    -/ Click on the followers tab
-    -/ click the "Snag Usernames" Button
-    -/ It will scroll for a bit
-    -/ It will download a text file with the usernames
-    ---//some stray links may be at the top so be careful
-    ---// i think i got them all ignored but you never know
-    
-    ---//USE AT OWN DISGRESSION
-    ---//IM NOT RESPONSIBLE IF YOUR ACCOUNT GETS BANNED
-    ---//DONT SUE ME.
-    ---//THIS PROJECT IS FOR EDUCATIONAL PURPOSES ONLY
-
-    Instagram Follower Scraper (For now)
-    // UP COMING FEATURES
-    ---/  Image and video scraping
-    ---/  Caption Scraping
-    ---/  Followings Scraping
-    ---/  Auto DM service
-    ---/  Comment scraping per post
-
-    (Plan on making one for twitter and tiktok too)
-
-    plan on making this a chrome extension in the near future
-
-    Designed By Ced
-    6/1/2023 2:12 AM
-    https://designedbyced.com
-    hire me plz.
-
-    Instagram: https://www.instagram.com/call.me.ceddy/
-    Twitter: 
-
-    please give stars so future employers think im cool :)
-*/
-
-
 // Function to scroll the div by class name
 function scrollDivByClassName(className) {
     // Set the duration for scrolling (in milliseconds)
@@ -76,9 +35,7 @@ function scrollDivByClassName(className) {
     }, 100);
   }
   
-  
-  // Function to copy the links and initiate download
- // Function to copy the links
+
 // Function to copy the links
 function copyLinks() {
     var links = document.querySelectorAll('a');
